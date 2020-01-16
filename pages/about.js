12a2aa47edd6ccfacbaae9837a9a19fe7ea8da84
joshua jackson <jjackson17mv@gmail.com>
2020-01-15 16:32:10 -0800

    push issues

---
+++ pages/about.js
@@ -0,0 +1,188 @@
+import React, { Component } from "react"
+import styled, { css, keyframes } from "styled-components";
+import { Flex, Box } from "reflexbox";
+import Layout from "../components/Layout/Layout";
+import nealImage from "../public/static/images/about/nealProfile.jpg"
+import Particles from 'react-particles-js'
+import backImage from "../public/static/images/about/redBlueback.jpg"
+
+const particleOptions = {
+    "particles": {
+        "number": {
+          "value": 80,
+          "density": {
+            "enable": true,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": "#ffffff"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "#000000"
+          },
+          "polygon": {
+            "nb_sides": 5
+          
+          }
+        },
+        "opacity": {
+          "value": 0.5,
+          "random": false,
+          "anim": {
+            "enable": false,
+            "speed": 1,
+            "opacity_min": 0.1,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 10,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 40,
+            "size_min": 0.1,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": true,
+          "distance": 300,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 2
+        },
+        "move": {
+          "enable": true,
+          "speed": 8,
+          "direction": "none",
+          "random": false,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": false,
+            "mode": "repulse"
+          },
+          "onclick": {
+            "enable": true,
+            "mode": "push"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 800,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 800,
+            "size": 40,
+            "duration": 2,
+            "opacity": 0.8,
+            "speed": 3
+          },
+          "repulse": {
+            "distance": 400,
+            "duration": 0.4
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 2
+          }
+        }
+      },
+      "retina_detect": true
+    }
+export default class about extends Component {
+    render() {
+        return (
+            <Layout>
+                
+                <Container>
+                
+                    <Flex justifyContent="center">
+                        <Box>
+                            <TextContainer>
+                                <AboutHeader>About Neal</AboutHeader>
+                            </TextContainer>
+                        </Box>
+                    </Flex>
+                    <Flex justifyContent="center" display="flex">
+                        <Box width={"75%"}>
+                            <ImgContainer>
+                                <Image src={nealImage}></Image>
+                            </ImgContainer>
+                        </Box>
+                        
+                        <Box width={"100%"}>
+                        <Particles params={particleOptions}/>
+                            <p style={{ color: 'white', fontSize: '30px',marginRight:'50px',paddingLeft:'27%' }}>
+                                All my life I have been blessed with a beautiful and
+                            interesting life. I’ve traveled extensively and lived in
+                            scenic and wonderful places. I’ve been surrounded by a
+                            loving family, lifelong friends and amazing
+                            opportunities. My art is a reflection of the wondrous
+                            life I have lived and I’m grateful every day for all the
+                            people and circumstances that have shaped my life.
+                            Welcome to my website and I hope you enjoy seeing my
+                            paintings as much as I have enjoyed creating them.
+                            </p>
+                        </Box>
+                        
+                    </Flex>
+                    
+                </Container>
+                
+            </Layout>
+            
+        )
+    }
+}
+
+
+const Container = styled.div`
+    background-image: url(${backImage});
+    padding-top: auto;
+    width:auto;
+    height:auto;
+    background-repeat: no-repeat;
+    background-size: cover;
+`;
+const ImgContainer = styled.div`
+    
+    padding-top: 40px;
+    margin-left:120px;
+`;
+const TextContainer = styled.div`
+    
+`;
+const AboutHeader = styled.h1`
+    
+    padding: 50px;
+    color: white;
+    font-size 50px;
+`;
+const Image = styled.img`
+    
+    height: auto;
+    width: 20vw;
+`;
